b246f33028fe9ff4c57bb42546e086fffee239c7
laurentpetit73 <petit.laurent73@gmail.com>
2020-10-12 20:04:23 +0200

    Mise en place pagination

--- src/Controller/AdminCommentController.php
+++ src/Controller/AdminCommentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Form\AdminCommentType;
 use App\Repository\CommentRepository;
+use App\Service\Pagination;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,12 +14,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AdminCommentController extends AbstractController
 {
      /**
-     * @Route("/admin/comments", name="admin_comments_index")
+     * @Route("/admin/comments/{page<\d+>?1}", name="admin_comments_index")
      */
-    public function index(CommentRepository $repo)
+    public function index($page , Pagination $pagination)
     {
+        $pagination->setEntityClass(Comment::class)
+            ->setLimit(5)
+            ->setCurrentPage($page);
+
         return $this->render('admin/comment/index.html.twig', [
-            'comments' => $repo->findAll(),
+            'pagination' => $pagination,
         ]);
     }
      /**
